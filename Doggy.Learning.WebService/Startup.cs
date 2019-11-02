aa20bb04c3ad50225363c6a0bbb39f3d3882242d
Sean Cheng <blackhorseya@gmail.com>
2019-11-02 13:42:32 +0800

    add db settings
    - add ConnectionStrings
    - add DbContextPool

--- Doggy.Learning.WebService/Startup.cs
+++ Doggy.Learning.WebService/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Doggy.Learning.Auth.Domain.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,12 @@ namespace Doggy.Learning.WebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
+
+            services.AddDbContextPool<AuthContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("auth"),
+                    b => b.MigrationsAssembly("Doggy.Learning.WebService")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,10 +46,7 @@ namespace Doggy.Learning.WebService
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
