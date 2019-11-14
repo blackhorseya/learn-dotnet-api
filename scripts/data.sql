9ccf689ea252b3d496532334270031b6cb36de84
Sean Cheng <blackhorseya@gmail.com>
2019-11-14 08:29:58 +0800

    fix deployment

--- scripts/data.sql
+++ scripts/data.sql
@@ -1,3 +1,3 @@
 insert into `Roles` (`Name`) values ('admin'), ('qa'), ('dev'), ('op');
 insert into `Groups` (`Name`) values ('user1'), ('user2'), ('user3'), ('user4');
-insert into `GroupRole` (`GroupId`,`RoleId`) value ('1', '1'), ('2', '2'), ('3', '3'), ('3', '4');
+insert into `GroupRole` (`GroupId`,`RoleId`) value ('1', '1'), ('2', '2'), ('3', '3'), ('3', '4'), ('4', '1'), ('4', '2'), ('4', '3'), ('4', '4');
