c83d3fcc33072d9a11065053624afc1accf193a6
Sean Cheng <blackhorseya@gmail.com>
2019-12-26 09:03:59 +0800

    move appsettings to entity

--- src/Doggy.Learning.Infrastructure/Helpers/JwtHelper.cs
+++ src/Doggy.Learning.Infrastructure/Helpers/JwtHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Doggy.Learning.Infrastructure.Entities;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
