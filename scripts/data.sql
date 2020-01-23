21c334b3e3140c96a390ba038ceedc6885349f16
Sean Cheng <blackhorseya@gmail.com>
2020-01-23 21:33:15 +0800

    fix account db endpoint

--- scripts/data.sql
+++ scripts/data.sql
@@ -5,6 +5,6 @@ insert into `Modules` (`Name`) values ('key'), ('management');
 insert into `Services` (`Name`) values ('ddi'), ('wfbss'), ('tippingpoint');
 
 -- relationship
-insert into `GroupRoleMap` (`GroupId`,`RoleId`) values ('1', '1'), ('2', '2'), ('2', '3'), ('3', '4');
-insert into `RoleModuleMap` (`RoleId`, `ModuleId`) values ('1', '2'), ('1', '1'), ('2', '1'), ('4', '1'), ('5', '1');
-insert into `RoleServiceMap` (`RoleId`, `ServiceId`) values ('2', '2'), ('3', '2'), ('4', '1'), ('5', '3');
+insert into `GroupRoleMap` (`GroupId`,`RoleId`) values ('2', '2'), ('5', '5'), ('5', '8'), ('8', '11');
+insert into `RoleModuleMap` (`RoleId`, `ModuleId`) values ('2', '5'), ('2', '2'), ('5', '2'), ('11', '2'), ('14', '2');
+insert into `RoleServiceMap` (`RoleId`, `ServiceId`) values ('5', '5'), ('8', '5'), ('11', '2'), ('14', '8');
